c43a5236a290b8948c1a3cddad3d67746312e87c
JeffLi1993 <qiangqiangli1993@gmail.com>
2017-04-21 16:45:50 +0800

    NN 算法

---
+++ org/ml/algorithm/nn/VisulizeDigits.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+print(digits.data.shape)
+
+import pylab as pl
+
+pl.gray()
+pl.matshow(digits.images[0])
+pl.show()
